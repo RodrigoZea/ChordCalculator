a616f8e076387dfcaa8ced61c169bf13dc987d8d
RodrigoZea <zearodrigo37@gmail.com>
2021-08-05 21:03:29 -0600

    Haciendo el calculo correcto

--- main.py
+++ main.py
@@ -27,30 +27,60 @@ def generateMajorKey(rootNote):
     return majorKey
          
 def generateChords(majorKey):
+    chords = []
     for i in range(0, len(majorKey)):
         chordPos = [i, i+2, i+4]
         chord = []
 
         for pos in chordPos:
             chord.append(majorKey[pos%7])
+ 
+        chords.append(chord)
 
-        if (i == 0 or i == 3 or i == 4):
-            print(chord, " - Mayor")
-        elif (i == 1 or i == 2 or i == 5):
-            print(chord, " - Menor")
-        elif (i == 6):
-            print(chord, "- Disminuido")
-        else:
-            print(chord)  
+    return chords 
 
-#['Do', 'Re', 'Mi', 'Fa', 'Sol', 'La', 'Si']
-#mayor: Do Fa Sol
-#menor: Re menor Mi Menor La menor
-#disminuido: Si
 
-#['Fa#', 'Sol#', 'La#', 'Si', 'Do#', 'Re#', 'Fa']
-#mayor: Fa# Si Do#
-#menor:
+def generateQualityChords(chords):
+    quality = ""
+    for chord in chords:
+        firstDistance = notesArray.index(chord[0]) - notesArray.index(chord[1])
+        secondDistance = notesArray.index(chord[1]) - notesArray.index(chord[2])
+
+        if firstDistance == 9:
+            firstDistance = abs(firstDistance-12)
+        if secondDistance == 9:
+            secondDistance = abs(secondDistance-12)
+
+        firstDistance = abs(firstDistance)
+        secondDistance = abs(secondDistance)
+
+        quality = getQuality(firstDistance, secondDistance)
+        print(chord, quality)
+
+def getQuality(firstDistance, secondDistance):
+    quality = ""
+    if (firstDistance == 4 and secondDistance == 3):
+        quality = "Mayor"
+    elif (firstDistance == 3 and secondDistance == 4):
+        quality = "Menor"
+    elif (firstDistance == 3 and secondDistance == 3):
+        quality = "Disminuido"
+    elif (firstDistance == 4 and secondDistance == 4):
+        quality = "Aumentado"
+    else:
+        quality = "Ninguno"
+
+    return quality
+
+"""
+['Do' - 4 - 'Mi' - 3 - 'Sol']
+['Re' - 3 - 'Fa' - 4 - 'La']
+['Mi' - 3 'Sol' - 4 - 'Si']
+['Fa' - 4 - 'La' - 3 - 'Do']
+['Sol' - 4 - 'Si' - 3 - 'Re']
+['La' - 3 - 'Do' - 4 - 'Mi']
+['Si' - 3 - 'Re' - 3 - 'Fa']
+"""
 
 def main():
     startingNote = input("Ingrese la nota inicial (0: Do, 1: Do, ..., 11: Si): ")
@@ -60,7 +90,10 @@ def main():
         majorKey = generateMajorKey(int(startingNote))
         print(majorKey)
 
+        print("----------------------------------------------------------")
+
         print("ACORDES:")
         chords = generateChords(majorKey)
+        generateQualityChords(chords)
 
 main()
